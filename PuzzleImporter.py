005f7f59588ea51feed4cc62149943e6b2bcfcff
Kieran <ringel.kieran@gmail.com>
2021-09-20 17:25:11 -0600

    Comments

--- PuzzleImporter.py
+++ PuzzleImporter.py
@@ -7,35 +7,41 @@ class PuzzleImporter():
         self.file = file
 
     def open(self):
+        """Kieran Ringel
+        Opens csv and gets rid of extra letters at beginning of [0][0]"""
         file = open(self.file, 'r')  # opens file
-        numpy_array = np.loadtxt(file, dtype=str, delimiter=",")
-        numpy_array[0][0] = numpy_array[0][0][-1]
+        numpy_array = np.loadtxt(file, dtype=str, delimiter=",")    #puts in 2d array
+        numpy_array[0][0] = numpy_array[0][0][-1]   #removes unnecessary letters
         return numpy_array
 
     def PrintPuzzle(puzzle):
+        """Mason Medina
+        Iterates through puzzle and prints in simple to read way"""
         print("\n")
         for i in range(len(puzzle)):
             line = ""
             if i == 3 or i == 6:
-                print("---------------------")
+                print("---------------------")  #every 3 rows print a line
             for j in range(len(puzzle[i])):
                 if j == 3 or j == 6:
-                    line += "| "
+                    line += "| "                #every 3 columns print a divider
                 line += str(puzzle[i][j]) + " "
             print(line)
 
     def puzzle_formating(array):
+        """Mason Medina
+        Changes puzzle from char array to int array replacing ? with 0"""
         for i in range(0, 9):
             for j in range(0, 9):
-                if array[i, j] == '?':
-                    array[i, j] = 0
+                if array[i, j] == '?':  #if its a ?
+                    array[i, j] = 0     #replace with 0
 
-        puzzle = np.zeros((9, 9), dtype=int)
+        puzzle = np.zeros((9, 9), dtype=int)    #create 9x9 of 0s
         for i in range(9):
             for j in range(9):
-                puzzle[i][j] = array[i][j]
+                puzzle[i][j] = array[i][j]      #copy array onto puzzle
 
-        empty_puzzle = puzzle.copy()
+        empty_puzzle = puzzle.copy()        #copy puzzle
 
         for i in range(0, 9)[::3]:
             for j in range(0, 9)[::3]:
