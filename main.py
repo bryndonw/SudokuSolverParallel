24086a01a72bb63bb580b48419a731df05eedf66
bryndonw <bryndonw99@gmail.com>
2021-09-20 19:23:46 -0600

    Main calling backtracking

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from Backtracking import Backtracking
 from PuzzleImporter import PuzzleImporter
 from GA import GA
 from SimulatedAnnealing import simulated_annealing
@@ -5,18 +6,24 @@ from SimulatedAnnealing import simulated_annealing
 
 def main():
     """"Puzzle Selection"""
-    puzzle = PuzzleImporter('Data/Med-P1.csv')
+    puzzle = PuzzleImporter('Data/Easy-P1.csv')
     array = puzzle.open()
 
     """BackTracking Algorithm Run"""
+    print("Simple backtracking with static heuristics: ")
+    Backtracking(array, 0)
+    print("Backtracking with forward checking and random heuristics: ")
+    Backtracking(array, 1)
+    print("Backtracking with arc-consistency and minimum remaining value heuristics: ")
+    Backtracking(array, 2)
 
 
     """Genetic Algorithm Run"""
-    GA(array)
+    #GA(array)
 
     """"Simulated Annealing Algorithm Run"""
-    #empty_puzzle, puzzle = PuzzleImporter.puzzleFormating(array)
-    #PuzzleImporter.PrintPuzzle(simulated_annealing(empty_puzzle, puzzle))
+  #  empty_puzzle, puzzle = PuzzleImporter.puzzleFormating(array)
+   # PuzzleImporter.PrintPuzzle(simulated_annealing(empty_puzzle, puzzle))
 
 
 if __name__ == '__main__':
